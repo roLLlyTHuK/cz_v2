c6abeb35cf043727fa91ad1eb79744125da70cde
roLLlyTHuK <ircvipua@gmail.com>
2024-02-12 14:13:11 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
-AOS.init();
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1,
+  navigation: true,
+  pagination: true,
+  mousewheel: {
+    forceToAxis: true,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+AOS.init({
+  once: true,
+});
